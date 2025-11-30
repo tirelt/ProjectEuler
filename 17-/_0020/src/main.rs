8ae4cec7886da9ce528f868db8f6c5812501dddd
theo <theotirel@gmail.com>
2025-11-30 22:43:53 +0800

    auto format

--- 17-/_0020/src/main.rs
+++ 17-/_0020/src/main.rs
@@ -1,22 +1,20 @@
 fn main() {
     let n = 100;
-    let number_digit: f64 = (1..=n).map(|x| (x as f64).ln()/(10 as f64).ln()).sum();
+    let number_digit: f64 = (1..=n).map(|x| (x as f64).ln() / (10 as f64).ln()).sum();
     println!("Approx numver of digit of 100! {number_digit}");
     let mut digit = vec![1];
-
-    for i in 1..=n{
-        let mut remainder = 0; 
+    for i in 1..=n {
+        let mut remainder = 0;
         for v in digit.iter_mut() {
             remainder += *v * i;
             *v = remainder % 10;
             remainder = remainder / 10;
         }
         while remainder > 0 {
-            digit.push(remainder%10);
-            remainder = remainder/10;
+            digit.push(remainder % 10);
+            remainder = remainder / 10;
         }
     }
     let res: u32 = digit.iter().sum();
     println!("{res}");
 }
-
