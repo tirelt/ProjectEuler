7d89fe4645338e2b95565a903ac428ed96122d33
theo <theotirel@gmail.com>
2025-12-13 00:09:24 +0800

    save

--- 17-/_0024/src/main.rs
+++ 17-/_0024/src/main.rs
@@ -8,7 +8,9 @@ fn get_fact(n: u64, table: &mut Vec<u64>) {
     }
 }
 fn main() {
+    let target: u64 = 1_000_000;
     let mut table = Vec::new();
     let test = get_fact(10, &mut table);
+    let first = target / &table[9];
     println!("TEst");
 }
