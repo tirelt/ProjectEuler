e19c278647f7c2cf90dddc9e67883679f82ee4e4
theo <theotirel@gmail.com>
2025-12-14 20:16:48 +0800

    need to debug and test 24

--- 17-/_0024/src/main.rs
+++ 17-/_0024/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::BTreeMap;
 
 fn get_fact(n: u64, table: &mut Vec<u64>) {
     match n {
@@ -9,21 +9,47 @@ fn get_fact(n: u64, table: &mut Vec<u64>) {
         }
     }
 }
+fn find_number(n: u64, numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
+    let mut counter = 1;
+    for (num, available) in numbers {
+        if *available {
+            if counter == n {
+                *available = false;
+                return Some(*num);
+            }
+            counter += 1;
+        }
+    }
+    return None;
+}
+fn last_number(numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
+    for (num, available) in numbers.iter_mut().rev() {
+        if *available {
+            *available = false;
+            return Some(*num);
+        }
+    }
+    None
+}
 fn main() {
-    let target: u64 = 1_000_000;
+    let mut target: u64 = 1_000_000;
     let mut table = Vec::new();
     get_fact(10, &mut table);
-    let mut numbers = BTreeSet::new();
-    for i in 0..9 {
-        numbers.insert(i);
+    let mut numbers = BTreeMap::new();
+    for i in 0..=9 {
+        numbers.insert(i, true);
     }
-    let mut num = 0;
-    let mut counter = 10;
-    let mut curr_number = 0;
     let mut res = 0;
+    let mut num;
     for i in (0..=9).rev() {
-        let first = target / &table[i];
-        curr_number += first * &table[i];
-        res += first * (10 as u32).pow(i);
+        if target > 0 {
+            let first = target / &table[i];
+            num = find_number(first, &mut numbers).unwrap();
+            target -= first * &table[i];
+        } else {
+            num = last_number(&mut numbers).unwrap();
+        }
+        res += num * (10 as u64).pow(i as u32);
     }
+    println!("Res: {res}");
 }
