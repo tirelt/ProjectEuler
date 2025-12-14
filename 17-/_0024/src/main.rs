b8ea274071fa78101bde28cd62a76c9092782079
theo <theotirel@gmail.com>
2025-12-14 14:13:32 +0800

    blblab

--- 17-/_0024/src/main.rs
+++ 17-/_0024/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 fn get_fact(n: u64, table: &mut Vec<u64>) {
     match n {
         0 => table.push(1),
@@ -10,7 +12,18 @@ fn get_fact(n: u64, table: &mut Vec<u64>) {
 fn main() {
     let target: u64 = 1_000_000;
     let mut table = Vec::new();
-    let test = get_fact(10, &mut table);
-    let first = target / &table[9];
-    println!("TEst");
+    get_fact(10, &mut table);
+    let mut numbers = BTreeSet::new();
+    for i in 0..9 {
+        numbers.insert(i);
+    }
+    let mut num = 0;
+    let mut counter = 10;
+    let mut curr_number = 0;
+    let mut res = 0;
+    for i in (0..=9).rev() {
+        let first = target / &table[i];
+        curr_number += first * &table[i];
+        res += first * (10 as u32).pow(i);
+    }
 }
