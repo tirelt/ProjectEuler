04e1ebd2affe4f8814a29eb82318d8c67a8ff868
theo <theotirel@gmail.com>
2025-12-15 19:06:35 +0800

    fixed 24

--- 17-/_0024/src/main.rs
+++ 17-/_0024/src/main.rs
@@ -10,7 +10,7 @@ fn get_fact(n: u64, table: &mut Vec<u64>) {
     }
 }
 fn find_number(n: u64, numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
-    let mut counter = 1;
+    let mut counter = 0;
     for (num, available) in numbers {
         if *available {
             if counter == n {
@@ -22,8 +22,8 @@ fn find_number(n: u64, numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
     }
     return None;
 }
-fn last_number(numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
-    for (num, available) in numbers.iter_mut().rev() {
+fn first_number(numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
+    for (num, available) in numbers.iter_mut() {
         if *available {
             *available = false;
             return Some(*num);
@@ -31,23 +31,26 @@ fn last_number(numbers: &mut BTreeMap<u64, bool>) -> Option<u64> {
     }
     None
 }
+
 fn main() {
-    let mut target: u64 = 1_000_000;
+    let mut target: u64 = 1_000_000 - 1;
+    let n_digit = 9;
     let mut table = Vec::new();
-    get_fact(10, &mut table);
+    get_fact(n_digit, &mut table);
     let mut numbers = BTreeMap::new();
-    for i in 0..=9 {
+    for i in 0..=n_digit {
         numbers.insert(i, true);
     }
     let mut res = 0;
     let mut num;
-    for i in (0..=9).rev() {
+    for i in (0..=n_digit).rev() {
+        let i = i as usize;
         if target > 0 {
             let first = target / &table[i];
             num = find_number(first, &mut numbers).unwrap();
             target -= first * &table[i];
         } else {
-            num = last_number(&mut numbers).unwrap();
+            num = first_number(&mut numbers).unwrap();
         }
         res += num * (10 as u64).pow(i as u32);
     }
