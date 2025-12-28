a75fee6fd72b9be4e4ac683df849824bb13cec27
theo <theotirel@gmail.com>
2025-12-28 17:53:31 +0800

    69

--- src/_0069/src/main.rs
+++ src/_0069/src/main.rs
@@ -1,26 +1,40 @@
 use pe_lib::primes::find_primes_sieve;
-use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 fn main() {
     let start = Instant::now();
     let max_n = 1_000_000;
     let primes = find_primes_sieve(max_n);
-    let mut map: HashMap<u64, HashSet<u64>> = HashMap::new();
+    let mut res = 0;
+    let mut max_ratio = 0.0;
     for n in 2..max_n {
-        let sqrt = (n as f64).sqrt() as u64;
-        let mut not_primes = HashSet::new();
-        for p in primes.iter() {
-            if *p > sqrt {
-                break;
-            }
-            if n % p == 0 {
-                let entry = map.entry(*p).or_insert(HashSet::new());
-                if entry.len() > 0 {
-                    not_primes = not_primes.union(entry).cloned().collect()
+        let mut primes_divisor = Vec::new();
+        let mut n_not_primes = 0;
+        if primes.binary_search(&n).is_err() {
+            let mut n_ = n;
+            for p in primes.iter() {
+                if n % p == 0 {
+                    n_not_primes += (n - 1) / p;
+                    for c in primes_divisor.iter() {
+                        n_not_primes -= (n - 1) / (*c * p);
+                    }
+                    primes_divisor.push(*p);
+                    while n_ % p == 0 {
+                        n_ /= p;
+                    }
+                    if n_ == 1 {
+                        break;
+                    }
                 }
-                entry.insert(n);
             }
         }
+        let n_primes = n - 1 - n_not_primes;
+        let ratio = n as f64 / n_primes as f64;
+        println!("n={n},ratio={ratio},primes_divisors={:?}", primes_divisor);
+        if ratio > max_ratio {
+            res = n;
+            max_ratio = ratio;
+        }
     }
+    println!("Res: {res}");
     println!("Duration: {}ms", start.elapsed().as_millis())
 }
