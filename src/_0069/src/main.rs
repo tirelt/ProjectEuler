b44f96d8d1c618479a214d29459f64517a9e4860
theo <theotirel@gmail.com>
2025-12-29 18:04:00 +0800

    smart optimization for 69

--- src/_0069/src/main.rs
+++ src/_0069/src/main.rs
@@ -4,40 +4,43 @@ fn main() {
     let start = Instant::now();
     let max_n = 1_000_000;
     let primes = find_primes_sieve(max_n);
+    println!("Duration sieves: {}ms", start.elapsed().as_millis());
+    let mut primes_divisors_sieve = vec![Vec::new(); max_n as usize + 1];
+    for p in primes.iter() {
+        let mut n = p.clone();
+        while n <= max_n {
+            primes_divisors_sieve[n as usize].push(*p);
+            n += p;
+        }
+    }
+    println!("Duration primes sieves: {}ms", start.elapsed().as_millis());
     let mut res = 0;
     let mut max_ratio = 0.0;
+    //let mut primes_divisor = &Vec::new();
     for n in 2..=max_n {
-        let mut primes_divisor = Vec::new();
-        let mut n_not_primes = 0;
+        let mut n_divisors = 0;
         if primes.binary_search(&n).is_err() {
-            let mut n_ = n;
-            for p in primes.iter() {
-                if n % p == 0 {
-                    n_not_primes += (n - 1) / p;
-                    for c in primes_divisor.iter() {
-                        n_not_primes -= (n - 1) / (*c * p);
-                    }
-                    primes_divisor.push(*p);
-                    while n_ % p == 0 {
-                        n_ /= p;
-                    }
-                    if n_ == 1 {
-                        break;
-                    }
+            let primes_divisor = &primes_divisors_sieve[n as usize];
+            for i in 0..primes_divisor.len() {
+                n_divisors += (n - 1) / primes_divisor[i];
+                for j in (i + 1)..primes_divisor.len() {
+                    n_divisors -= (n - 1) / (primes_divisor[i] * primes_divisor[j]);
                 }
             }
         }
-        let n_primes = n - 1 - n_not_primes;
-        let ratio = n as f64 / n_primes as f64;
+        let phi = n - 1 - n_divisors;
+        let ratio = n as f64 / phi as f64;
+        /*
         println!(
-            "n={n}, ratio={ratio:.2}, phi(n)={n_primes}, primes_divisors={:?}",
+            "n={n}, ratio={ratio:.2}, phi(n)={phi}, primes_divisors={:?}",
             primes_divisor
         );
+        */
         if ratio > max_ratio {
             res = n;
             max_ratio = ratio;
         }
     }
     println!("Res: {res}");
-    println!("Duration: {}ms", start.elapsed().as_millis())
+    println!("Duration: {}ms", start.elapsed().as_millis());
 }
